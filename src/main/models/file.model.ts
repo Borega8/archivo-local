b8c75fd0e8c09ed0636ba146629049b609502f96
Borega8 <borega888@gmail.com>
2025-01-14 17:30:22 -0600

    feat(files): :sparkles: create file model

---
+++ src/main/models/file.model.ts
@@ -0,0 +1,66 @@
+import { prisma } from '../constants/database'
+import { FileType } from '../constants/file'
+import { TFile } from '../types/File'
+import { ReturnValue } from '../types/ReturnValue'
+
+export class FileModel {
+  static async create(fileData: TFile, type: FileType): Promise<ReturnValue<TFile>> {
+    try {
+      if (type == FileType.RECEIVED) {
+        const file = await prisma.documentosRecibidos.create({
+          data: {
+            asunto: fileData.asunto,
+            atn: fileData.atn,
+            codigo_clasificacion: fileData.codigo_clasificacion,
+            dependencia: fileData.dependencia,
+            estado: fileData.estado,
+            fecha_oficio: fileData.fecha_oficio,
+            fecha_recibido: fileData.fecha_recibido,
+            file_path: fileData.file_path,
+            no_oficio: fileData.no_oficio,
+            nombre: fileData.nombre,
+            observaciones: fileData.observaciones,
+            para: fileData.para,
+            quien_firma: fileData.quien_firma,
+            quien_recibe: fileData.quien_recibe,
+            turnado: fileData.turnado,
+            ubicacion: fileData.ubicacion
+          }
+        })
+
+        await prisma.$disconnect()
+
+        return { data: file as TFile }
+      } else if (type == FileType.SENT) {
+        const file = await prisma.documentosEnviados.create({
+          data: {
+            asunto: fileData.asunto,
+            atn: fileData.atn,
+            codigo_clasificacion: fileData.codigo_clasificacion,
+            dependencia: fileData.dependencia,
+            estado: fileData.estado,
+            fecha_oficio: fileData.fecha_oficio,
+            fecha_recibido: fileData.fecha_recibido,
+            file_path: fileData.file_path,
+            no_oficio: fileData.no_oficio,
+            nombre: fileData.nombre,
+            observaciones: fileData.observaciones,
+            para: fileData.para,
+            quien_firma: fileData.quien_firma,
+            quien_recibe: fileData.quien_recibe,
+            quien_elaboro: fileData.quien_elaboro,
+            ubicacion: fileData.ubicacion
+          }
+        })
+
+        await prisma.$disconnect()
+
+        return { data: file as TFile }
+      }
+    } catch (error) {
+      return { error: new Error('Unable to save the document'), status: 500 }
+    }
+
+    return { error: new Error('Incorrect file type'), status: 400 }
+  }
+}
