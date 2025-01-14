6823ded392ebff5aa1305b803763716cd487cef5
Borega8 <borega888@gmail.com>
2025-01-14 16:13:09 -0600

    feat(files): create file controller class

---
+++ src/main/controllers/file.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+
+export class FileController {
+  static async getById(req: Request, res: Response) {}
+
+  static async getAll(req: Request, res: Response) {}
+
+  static async create(req: Request, res: Response) {}
+
+  static async update(req: Request, res: Response) {}
+
+  static async deleteById(req: Request, res: Response) {}
+}
