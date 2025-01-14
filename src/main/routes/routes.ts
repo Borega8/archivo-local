0d323e167c9872cd230f288fdb966541420a96aa
Borega8 <borega888@gmail.com>
2025-01-14 16:14:22 -0600

    feat(files): add files routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express'
+import { FileController } from '../controllers/file.controller'
 
 const router = Router()
+
+router.get('/files', FileController.getAll)
+router.get('/files/:id', FileController.getById)
+router.post('/files', FileController.create)
+router.put('/files/:id', FileController.update)
+router.delete('/files/:id', FileController.deleteById)
 
 export default router
