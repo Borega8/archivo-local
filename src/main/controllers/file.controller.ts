77c4b0abf3a067fc561a4bf66325e243e5143351
Borega8 <borega888@gmail.com>
2025-01-15 17:14:21 -0600

    feat(files): :sparkles: update file data

--- src/main/controllers/file.controller.ts
+++ src/main/controllers/file.controller.ts
@@ -90,7 +90,78 @@ export class FileController {
       : res.status(201).json(data)
   }
 
-  static async update(req: Request, res: Response) {}
+  static async update(req: Request, res: Response) {
+    const { id } = req.params
+    const {
+      type,
+      dependency,
+      noFile,
+      subject,
+      dateFile,
+      to,
+      atn,
+      whoSigns,
+      whoReceived,
+      createdBy,
+      dateReceived,
+      turn,
+      status,
+      serieCode,
+      location,
+      observations,
+      fileName
+    }: {
+      type: FileType
+      dependency: string
+      noFile: string
+      subject: string
+      dateFile: string
+      to: string
+      atn: string
+      whoSigns: string
+      whoReceived: string
+      createdBy: string
+      dateReceived: string
+      turn: string
+      status: string
+      serieCode: string
+      location: string
+      observations: string
+      fileName: string
+    } = req.body
+
+    const {
+      data,
+      error,
+      status: statusCode
+    } = await FileModel.update(
+      {
+        asunto: subject,
+        atn: atn,
+        codigo_clasificacion: serieCode,
+        dependencia: dependency,
+        estado: status,
+        fecha_oficio: dateFile,
+        fecha_recibido: dateReceived,
+        no_oficio: noFile,
+        nombre: fileName,
+        observaciones: observations,
+        para: to,
+        quien_elaboro: createdBy,
+        quien_firma: whoSigns,
+        quien_recibe: whoReceived,
+        turnado: turn,
+        ubicacion: location,
+        file_path: '',
+        documento_id: Number(id)
+      } as TFile,
+      type
+    )
+
+    return error
+      ? res.status(statusCode).json({ error: error.message })
+      : res.status(200).json(data)
+  }
 
   static async deleteById(req: Request, res: Response) {}
 }
--- src/main/models/file.model.ts
+++ src/main/models/file.model.ts
@@ -106,4 +106,82 @@ export class FileModel {
 
     return { error: new Error('Incorrect file type'), status: 400 }
   }
+
+  static async update(fileData: TFile, type: FileType): Promise<ReturnValue<TFile>> {
+    try {
+      if (type == FileType.RECEIVED) {
+        const fileFound = await prisma.documentosRecibidos.findUnique({
+          where: { documento_id: fileData.documento_id }
+        })
+
+        if (!fileFound) return { error: new Error('File not found'), status: 404 }
+
+        const file = await prisma.documentosRecibidos.update({
+          where: {
+            documento_id: fileData.documento_id
+          },
+          data: {
+            asunto: fileData.asunto,
+            atn: fileData.atn,
+            codigo_clasificacion: fileData.codigo_clasificacion,
+            dependencia: fileData.dependencia,
+            estado: fileData.estado,
+            fecha_oficio: fileData.fecha_oficio,
+            fecha_recibido: fileData.fecha_recibido,
+            file_path: fileData.file_path,
+            no_oficio: fileData.no_oficio,
+            nombre: fileData.nombre,
+            observaciones: fileData.observaciones,
+            para: fileData.para,
+            quien_firma: fileData.quien_firma,
+            quien_recibe: fileData.quien_recibe,
+            turnado: fileData.turnado,
+            ubicacion: fileData.ubicacion
+          }
+        })
+
+        await prisma.$disconnect()
+
+        return { data: file as TFile }
+      } else if (type == FileType.SENT) {
+        const fileFound = await prisma.documentosEnviados.findUnique({
+          where: { documento_id: fileData.documento_id }
+        })
+
+        if (!fileFound) return { error: new Error('File not found'), status: 404 }
+
+        const file = await prisma.documentosEnviados.update({
+          where: {
+            documento_id: fileData.documento_id
+          },
+          data: {
+            asunto: fileData.asunto,
+            atn: fileData.atn,
+            codigo_clasificacion: fileData.codigo_clasificacion,
+            dependencia: fileData.dependencia,
+            estado: fileData.estado,
+            fecha_oficio: fileData.fecha_oficio,
+            fecha_recibido: fileData.fecha_recibido,
+            file_path: fileData.file_path,
+            no_oficio: fileData.no_oficio,
+            nombre: fileData.nombre,
+            observaciones: fileData.observaciones,
+            para: fileData.para,
+            quien_firma: fileData.quien_firma,
+            quien_recibe: fileData.quien_recibe,
+            quien_elaboro: fileData.quien_elaboro,
+            ubicacion: fileData.ubicacion
+          }
+        })
+
+        await prisma.$disconnect()
+
+        return { data: file as TFile }
+      }
+    } catch (error) {
+      return { error: new Error('Unable to update the document'), status: 500 }
+    }
+
+    return { error: new Error('Incorrect file type'), status: 400 }
+  }
 }
