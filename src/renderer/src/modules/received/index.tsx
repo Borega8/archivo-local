b98925946ae989c76106f4aae1b254c39fb9d471
Borega8 <borega888@gmail.com>
2025-01-16 17:31:05 -0600

    feat(files): :sparkles: fetch all received files

--- src/renderer/src/modules/received/index.tsx
+++ src/renderer/src/modules/received/index.tsx
@@ -1,3 +1,30 @@
+import { FileType } from '@renderer/constants/file'
+import { SERVER_URL } from '@renderer/constants/routes'
+import { useQuery } from '@tanstack/react-query'
+
+const getFiles = async (fileType: FileType) => {
+  const response = await fetch(`${SERVER_URL}/files?type=${fileType}`)
+  const data = await response.json()
+
+  if (data.error) throw new Error(data.error)
+
+  return data
+}
+
+export function useGetFiles(fileType: FileType) {
+  return useQuery({
+    queryKey: ['files', fileType],
+    queryFn: () => getFiles(fileType)
+  })
+}
+
 export function Received() {
+  const { data: files, isLoading, error } = useGetFiles(FileType.RECEIVED)
+
+  isLoading && <h3>Locading...</h3>
+  error && <h3>{error.message}</h3>
+
+  console.log(files)
+
   return <div>Received</div>
 }
