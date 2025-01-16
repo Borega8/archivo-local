451dff3df5170c64a1974b29f04895168d830ad7
Borega8 <borega888@gmail.com>
2025-01-15 18:19:54 -0600

    style: implements AdapterDateFns as date adapter

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './utils/theme'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+      </LocalizationProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
