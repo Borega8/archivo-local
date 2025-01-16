e3087913d9a7ba85ab83df31ee6089797597e49e
Borega8 <borega888@gmail.com>
2025-01-15 19:18:23 -0600

    style(components): :lipstick: create navigation element component

---
+++ src/renderer/src/components/NavigationElement/NavigationElement.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom'
+import { ReactNode } from 'react'
+
+type NavigationElementProps = {
+  currentPath: string
+  to: string
+  label: string
+  iconActive: ReactNode
+  iconInactive: ReactNode
+}
+
+export function NavigationElement({
+  currentPath,
+  to,
+  label,
+  iconActive,
+  iconInactive
+}: NavigationElementProps) {
+  const isCurrentPage = currentPath === to || currentPath.includes(to)
+  return (
+    <NavLink
+      to={to}
+      className={isCurrentPage ? 'link label-medium-prominent' : 'link label-medium'}
+    >
+      <span className={isCurrentPage ? 'mui-icon icon-active' : 'mui-icon'}>
+        {isCurrentPage ? iconActive : iconInactive}
+      </span>
+      {label}
+    </NavLink>
+  )
+}
