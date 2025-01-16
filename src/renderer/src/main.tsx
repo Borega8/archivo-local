e73ff5197b364a1adce99296e5b88bc506e603f0
Borega8 <borega888@gmail.com>
2025-01-16 17:02:15 -0600

    feat(config): add query client provider

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './utils/theme'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './constants/queryClient'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
