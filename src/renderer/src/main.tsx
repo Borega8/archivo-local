f867b28da4019ad5f355a944e8d479b9454d1596
Borega8 <borega888@gmail.com>
2025-01-15 18:17:03 -0600

    revert: :rewind: remove date adapter

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,15 +5,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './utils/theme'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <App />
-      </LocalizationProvider>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 )
