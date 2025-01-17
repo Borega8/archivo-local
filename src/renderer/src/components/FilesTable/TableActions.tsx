b9be70a1ab0ebe7f7de0f4174861fabe1fe2654d
Borega8 <borega888@gmail.com>
2025-01-16 18:00:38 -0600

    style(files): :lipstick: create table actions column component

---
+++ src/renderer/src/components/FilesTable/TableActions.tsx
@@ -0,0 +1,69 @@
+import { Delete, Edit, OpenInNew } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import { AppRoutesEnum } from '@renderer/constants/routes'
+import { TFile } from '@renderer/types/Files'
+import { useNavigate } from 'react-router-dom'
+
+export function TableActions({
+  cellData,
+  type,
+  handleOpen
+}: {
+  cellData: TFile
+  type: string
+  handleOpen: (file) => void
+}) {
+  return (
+    <span style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      <TableActionDelete
+        document_id={cellData.documento_id}
+        handleOpen={() => handleOpen(cellData)}
+      />
+      <TableActionEdit document_id={cellData.documento_id} type={type} />
+      <TableActionOpen url={cellData.file_path} />
+    </span>
+  )
+}
+
+function TableActionDelete({
+  document_id,
+  handleOpen
+}: {
+  document_id: number
+  handleOpen: (fileId: number) => void
+}) {
+  return (
+    <IconButton onClick={() => handleOpen(document_id)}>
+      <Delete sx={{ color: 'var(--md-sys-color-error)' }} />
+    </IconButton>
+  )
+}
+
+function TableActionEdit({ document_id, type }: { document_id: number; type: string }) {
+  const navigate = useNavigate()
+
+  return (
+    <IconButton
+      onClick={() =>
+        type === 'received'
+          ? navigate(`${AppRoutesEnum.RECEIVED}/${document_id}`)
+          : navigate(`${AppRoutesEnum.SENT}/${document_id}`)
+      }
+    >
+      <Edit sx={{ color: 'blue' }} />
+    </IconButton>
+  )
+}
+
+function TableActionOpen({ url }: { url: string }) {
+  return (
+    <IconButton
+      onClick={() => {
+        // window.electron.ipcRenderer.send('open-file', url)
+        open(url, '_blank')
+      }}
+    >
+      <OpenInNew sx={{ color: 'var(--md-sys-color-scrim)' }} />
+    </IconButton>
+  )
+}
