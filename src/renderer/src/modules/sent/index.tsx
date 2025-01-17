31fd9076312d0c53b3317c14abc3b1089fb00b09
Borega8 <borega888@gmail.com>
2025-01-16 18:50:37 -0600

    style(files): :lipstick: show files sent

--- src/renderer/src/modules/sent/index.tsx
+++ src/renderer/src/modules/sent/index.tsx
@@ -1,3 +1,44 @@
+import { AlertMessage, FilesTable, ProgressBar } from '@renderer/components'
+import { FileType } from '@renderer/constants/file'
+import { columnsFileReceived } from '@renderer/constants/tableColumns'
+import { useFilesTable } from '@renderer/hooks/useFilesTable'
+import { useGetFiles } from '@renderer/hooks/useGetFiles'
+import { ContentScrollableLayout } from '@renderer/layouts/ContentScrollableLayout'
+import { MainLayout } from '@renderer/layouts/MainLayout'
+
 export function Sent() {
-  return <div>Sent</div>
+  const { data: files, isFetching, isPending, error } = useGetFiles(FileType.SENT)
+
+  const { table } = useFilesTable({
+    files,
+    columnDef: columnsFileReceived,
+    columnVisibitily: {
+      ['name']: true,
+      ['dependency']: true,
+      ['no_file']: true,
+      ['subject']: true,
+      ['date_file']: true,
+      ['to']: true,
+      ['atn']: true,
+      ['who_signs']: true,
+      ['who_received']: true,
+      ['date_received']: true,
+      ['turn']: true,
+      ['status']: true,
+      ['serie_code']: true,
+      ['location']: true,
+      ['observations']: true
+    }
+  })
+
+  return (
+    <MainLayout title="Documentos de salida">
+      <ContentScrollableLayout>
+        <FilesTable table={table} type={FileType.SENT} handleOpen={null} />
+      </ContentScrollableLayout>
+
+      {(isFetching || isPending) && <ProgressBar />}
+      {error && <AlertMessage message={error.message} isError />}
+    </MainLayout>
+  )
 }
