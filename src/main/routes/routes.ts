2da5783a0751478fed7fbbdb858ab8dd30eae373
Borega8 <borega888@gmail.com>
2025-01-18 13:07:44 -0600

    feat(server): :sparkles: create files storage using multer

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,12 +1,36 @@
 import { Router } from 'express'
 import { FileController } from '../controllers/file.controller'
+import multer from 'multer'
+import { mkdir } from 'node:fs'
+import { getYear } from 'date-fns'
+import { FileType } from '../constants/file'
+import { BASE_DIR } from '../constants/basedir'
+
+const storage = multer.diskStorage({
+  destination: async (req, _, cb) => {
+    const year = getYear(req.body.dateFile)
+    const type: string = req.body.type
+
+    mkdir(`${BASE_DIR}`, (err) => err)
+    mkdir(`${BASE_DIR}/${year}`, (err) => err)
+
+    if (type === FileType.RECEIVED) {
+      mkdir(`${BASE_DIR}/${year}/DOCUMENTOS DE ENTRADA`, (err) => err)
+      cb(null, `${BASE_DIR}/${year}/DOCUMENTOS DE ENTRADA`)
+    } else {
+      mkdir(`${BASE_DIR}/${year}/DOCUMENTOS DE SALIDA`, (err) => err)
+      cb(null, `${BASE_DIR}/${year}/DOCUMENTOS DE SALIDA`)
+    }
+  }
+})
+const upload = multer({ storage: storage })
 
 const router = Router()
 
 router.get('/files', FileController.getAll)
 router.get('/files/:id', FileController.getById)
-router.post('/files', FileController.create)
-router.put('/files/:id', FileController.update)
+router.post('/files', upload.array('files'), FileController.create)
+router.put('/files/:id', upload.array('files'), FileController.update)
 router.delete('/files/:id', FileController.deleteById)
 
 export default router
