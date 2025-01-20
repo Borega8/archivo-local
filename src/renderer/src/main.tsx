1a064806e8e82411495abac5766af5b5ac815012
Borega8 <borega888@gmail.com>
2025-01-20 12:01:50 -0600

    fix(config): :ambulance: fix mui date adapter

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -7,13 +7,17 @@ import { ThemeProvider } from '@emotion/react'
 import { theme } from './utils/theme'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './constants/queryClient'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </LocalizationProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
