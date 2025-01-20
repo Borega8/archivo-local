d9e769f94a07cc6d668931668f94c6b1000f6c54
Borega8 <borega888@gmail.com>
2025-01-20 12:07:41 -0600

    style(components): :lipstick: add form to upload files

--- src/renderer/src/modules/sent/create/index.tsx
+++ src/renderer/src/modules/sent/create/index.tsx
@@ -1,3 +1,11 @@
+import { MainLayout } from '@renderer/layouts/MainLayout'
+import { DocSentForm } from '@renderer/modules/sent/components'
+
 export function NewSent() {
-  return <div>NewReceived</div>
+  return (
+    <MainLayout title="Nuevo documento de salida">
+      <h3 className="title-large">Datos del archivo</h3>
+      <DocSentForm />
+    </MainLayout>
+  )
 }
