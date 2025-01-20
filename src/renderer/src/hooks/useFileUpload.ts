39526a51e5b14d0b9ff9711bad6ff89d7ffff4f6
Borega8 <borega888@gmail.com>
2025-01-20 11:59:01 -0600

    feat(files): :sparkles: create custom hook to upload files

---
+++ src/renderer/src/hooks/useFileUpload.ts
@@ -0,0 +1,52 @@
+import { SERVER_URL } from '@renderer/constants/routes'
+import { TFile } from '@renderer/types/Files'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+
+const uploadFile = async (formData: FormData): Promise<TFile> => {
+  const response = await fetch(`${SERVER_URL}/files`, {
+    method: 'post',
+    body: formData
+  })
+
+  const data = await response.json()
+
+  if (data.error) throw new Error(data.error)
+
+  return data.file
+}
+
+export function useFileUpload() {
+  const [showAlert, setShowAlert] = useState(false)
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+
+    const file = await mutateAsync(form)
+    return file
+  }
+
+  const { data, isPending, isError, error, mutateAsync } = useMutation({
+    mutationKey: ['upload', 'file'],
+    mutationFn: (formData: FormData) => uploadFile(formData),
+    onSuccess: () => {
+      setShowAlert(true)
+
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    },
+    onError: () => {
+      setShowAlert(true)
+
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    }
+  })
+
+  return { data, isPending, isError, errorUploadFile: error, showAlert, uploadFile: onSubmit }
+}
