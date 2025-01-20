cd412372864f3dced3086283a3bee2f2ff529339
Borega8 <borega888@gmail.com>
2025-01-20 11:59:38 -0600

    style(files): :lipstick: add form to page

--- src/renderer/src/modules/received/pages/create/index.tsx
+++ src/renderer/src/modules/received/pages/create/index.tsx
@@ -1,3 +1,11 @@
+import { MainLayout } from '@renderer/layouts/MainLayout'
+import { DocReceivedForm } from '@renderer/modules/received/components'
+
 export function NewReceived() {
-  return <div>NewReceived</div>
+  return (
+    <MainLayout title="Nuevo documento de entrada">
+      <h3 className="title-large">Datos del archivo</h3>
+      <DocReceivedForm />
+    </MainLayout>
+  )
 }
