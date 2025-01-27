9cb8977cf3b5d9b7a28381daf30e66b013906503
Borega8 <borega888@gmail.com>
2025-01-27 13:16:11 -0600

    style(files): :lipstick: show file data in form

--- src/renderer/src/modules/received/pages/edit/index.tsx
+++ src/renderer/src/modules/received/pages/edit/index.tsx
@@ -1,3 +1,22 @@
+import { CustomAlert, ProgressBar } from '@renderer/components'
+import { FileType } from '@renderer/constants/file'
+import { useGetFile } from '@renderer/hooks/useGetFile'
+import { MainLayout } from '@renderer/layouts/MainLayout'
+import { DocReceivedForm } from '@renderer/modules/received/components'
+import { FileReceived } from '@renderer/types/Files'
+import { useParams } from 'react-router-dom'
+
 export function EditReceived() {
-  return <div>EditReceived</div>
+  const { id } = useParams()
+  const { data, isFetching, error } = useGetFile(Number(id), FileType.RECEIVED)
+
+  return (
+    <MainLayout title="Editar documento">
+      <h3 className="title-large">Datos del archivo</h3>
+      {data && <DocReceivedForm file={data as FileReceived} />}
+
+      {isFetching && <ProgressBar />}
+      {error && <CustomAlert text={error.message} severity="error" />}
+    </MainLayout>
+  )
 }
