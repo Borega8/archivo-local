0e2cb445d45901716c74b88b24d131c3c0ec1e47
Borega8 <borega888@gmail.com>
2025-01-27 13:39:38 -0600

    feat(files): :sparkles: update file data

--- src/renderer/src/modules/received/pages/edit/index.tsx
+++ src/renderer/src/modules/received/pages/edit/index.tsx
@@ -1,5 +1,6 @@
 import { CustomAlert, ProgressBar } from '@renderer/components'
 import { FileType } from '@renderer/constants/file'
+import { useEditFile } from '@renderer/hooks/useEditFile'
 import { useGetFile } from '@renderer/hooks/useGetFile'
 import { MainLayout } from '@renderer/layouts/MainLayout'
 import { DocReceivedForm } from '@renderer/modules/received/components'
@@ -9,14 +10,22 @@ import { useParams } from 'react-router-dom'
 export function EditReceived() {
   const { id } = useParams()
   const { data, isFetching, error } = useGetFile(Number(id), FileType.RECEIVED)
+  const {
+    isPending,
+    onSubmit,
+    showAlert,
+    error: errorEdit
+  } = useEditFile(Number(id), FileType.RECEIVED)
 
   return (
     <MainLayout title="Editar documento">
       <h3 className="title-large">Datos del archivo</h3>
-      {data && <DocReceivedForm file={data as FileReceived} />}
+      {data && <DocReceivedForm file={data as FileReceived} onSubmit={onSubmit} />}
 
-      {isFetching && <ProgressBar />}
+      {(isPending || isFetching) && <ProgressBar />}
       {error && <CustomAlert text={error.message} severity="error" />}
+      {showAlert && !errorEdit && <CustomAlert text="Archivo actualizado correctamente" />}
+      {showAlert && errorEdit && <CustomAlert text={errorEdit.message} severity="error" />}
     </MainLayout>
   )
 }
