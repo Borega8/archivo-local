311108ef42e966770041633fcbb973e3722874ab
Borega8 <borega888@gmail.com>
2025-01-27 13:14:52 -0600

    feat(files): :sparkles: create custom hook to get file data

---
+++ src/renderer/src/hooks/useGetFile.ts
@@ -0,0 +1,21 @@
+import { SERVER_URL } from '@renderer/constants/routes'
+import { TFile } from '@renderer/types/Files'
+import { useQuery } from '@tanstack/react-query'
+
+const getFile = async (id: number, type: string): Promise<TFile> => {
+  const response = await fetch(`${SERVER_URL}/files/${id}?type=${type}`)
+  const data = await response.json()
+
+  if (data.error) throw new Error(data.error)
+
+  return data
+}
+
+export function useGetFile(id: number, type: string) {
+  const { data, isFetching, error } = useQuery({
+    queryKey: ['file', type, id],
+    queryFn: () => getFile(id, type)
+  })
+
+  return { data, isFetching, error }
+}
