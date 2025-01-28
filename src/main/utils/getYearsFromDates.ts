04f913b495801fb4401a538ae0cac3b10b6c080d
Borega8 <borega888@gmail.com>
2025-01-28 12:39:51 -0600

    feat(files): sort array of years in descending order

--- src/main/utils/getYearsFromDates.ts
+++ src/main/utils/getYearsFromDates.ts
@@ -6,6 +6,7 @@ export function getYearsFromDates(dates: Dates[]): number[] {
   const years = dates
     .map((date) => Number(date.fecha_oficio.split('/').pop()))
     .filter((value, index, array) => array.indexOf(value) === index)
+    .toSorted((a, b) => b - a)
 
   return years
 }
