59e7002b9cf9c76101c4cc9c1f7b36e94da8845c
Borega8 <borega888@gmail.com>
2025-01-27 18:27:16 -0600

    feat(files): :sparkles: create custom hook to delete one file

---
+++ src/renderer/src/hooks/useDeleteFile.ts
@@ -0,0 +1,54 @@
+import { FileType } from '@renderer/constants/file'
+import { SERVER_URL } from '@renderer/constants/routes'
+import { TFile } from '@renderer/types/Files'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+
+const deleteFile = async (id: number, type: FileType): Promise<TFile> => {
+  const response = await fetch(`${SERVER_URL}/files/${id}?type=${type}`, {
+    method: 'delete'
+  })
+  const data = await response.json()
+
+  if (data.error) throw new Error(data.error)
+
+  return data
+}
+
+export function useDeleteFile(id: number, type: FileType) {
+  const [showAlert, setShowAlert] = useState(false)
+
+  const {
+    data: fileDeleted,
+    mutateAsync,
+    isPending,
+    isError,
+    error
+  } = useMutation({
+    mutationKey: ['file', type, id],
+    mutationFn: () => deleteFile(id, type),
+    onSuccess: () => {
+      setShowAlert(true)
+
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    },
+    onError: () => {
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    }
+  })
+
+  return {
+    fileDeleted,
+    deleteFile: mutateAsync,
+    isPending,
+    errorDeleteFile: error,
+    isError,
+    showAlert
+  }
+}
