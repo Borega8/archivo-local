2a108e8d931066f3ea87ff3036a36b230c21b5a6
Borega8 <borega888@gmail.com>
2025-01-31 18:57:09 -0600

    feat(files): :sparkles: create custom hook to create new serie

---
+++ src/renderer/src/modules/home/hooks/useAddSerie.ts
@@ -0,0 +1,51 @@
+import { SERVER_URL } from '@renderer/constants/routes'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+
+const addSerie = async (formData: FormData): Promise<TFile> => {
+  const response = await fetch(`${SERVER_URL}/series`, {
+    method: 'post',
+    body: formData
+  })
+
+  const data = await response.json()
+
+  if (data.error) throw new Error(data.error)
+
+  return data
+}
+
+export function useAddSerie({ refetchSeries }) {
+  const [showAlert, setShowAlert] = useState(false)
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+
+    await mutateAsync(form)
+    await refetchSeries()
+  }
+
+  const { data, isPending, isError, error, mutateAsync } = useMutation({
+    mutationKey: ['series', 'add'],
+    mutationFn: (formData: FormData) => addSerie(formData),
+    onSuccess: () => {
+      setShowAlert(true)
+
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    },
+    onError: () => {
+      setShowAlert(true)
+
+      const timeOut = setTimeout(() => {
+        setShowAlert(false)
+        clearTimeout(timeOut)
+      }, 5000)
+    }
+  })
+
+  return { data, isPending, isError, error, showAlert, onSubmit }
+}
