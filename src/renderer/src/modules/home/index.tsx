da9107a3334969583f2bf03e04c2cda9e8e88733
Borega8 <borega888@gmail.com>
2025-02-01 13:21:51 -0600

    style(files): :lipstick: add delete button to delete series

--- src/renderer/src/modules/home/index.tsx
+++ src/renderer/src/modules/home/index.tsx
@@ -1,14 +1,22 @@
-import { Add } from '@mui/icons-material'
-import { AlertMessage, CustomFAB, ProgressBar } from '@renderer/components'
+import { Add, Delete } from '@mui/icons-material'
+import { AlertMessage, ConfirmDialog, CustomFAB, ProgressBar } from '@renderer/components'
 import { MainLayout } from '@renderer/layouts/MainLayout'
 import { CreateSerieDialog } from './components'
 import { Fragment, useState } from 'react'
 import { useSeries } from '@renderer/modules/home/hooks/useSeries'
-import { Divider, ListItem, ListItemText } from '@mui/material'
+import { Divider, IconButton, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { useConfirmDialog } from '@renderer/hooks/useConfirmDialog'
 
 export function Home() {
   const { data: series, isFetching, refetch: refetchSeries, error } = useSeries()
   const [open, setOpen] = useState(false)
+  const [serie, setSerie] = useState<Serie>()
+  const {
+    open: isDialogOpen,
+    onAccept,
+    onClose,
+    openDialog
+  } = useConfirmDialog({ deleteFn: null, refetch: refetchSeries })
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -18,12 +26,29 @@ export function Home() {
     setOpen(false)
   }
 
+  const handleDialog = (serie: Serie) => {
+    setSerie(serie)
+    openDialog()
+  }
+
   return (
     <MainLayout title="Inicio">
+      <ConfirmDialog
+        open={isDialogOpen}
+        name={`${serie?.serie} ${serie?.titulo}`}
+        onAccept={onAccept}
+        onClose={onClose}
+      />
+
       {series?.map((serie) => (
         <Fragment key={serie.serie_id}>
           <ListItem>
             <ListItemText primary={`${serie.serie} ${serie.titulo}`} />
+            <ListItemIcon>
+              <IconButton onClick={() => handleDialog(serie)}>
+                <Delete sx={{ color: 'var(--md-sys-color-error)' }} />
+              </IconButton>
+            </ListItemIcon>
           </ListItem>
           <Divider />
         </Fragment>
