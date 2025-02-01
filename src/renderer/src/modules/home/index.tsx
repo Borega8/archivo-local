365df42d8640b60939c8882f595aef860f508360
Borega8 <borega888@gmail.com>
2025-02-01 13:16:09 -0600

    style(files): :lipstick: list series and add button to create new one

--- src/renderer/src/modules/home/index.tsx
+++ src/renderer/src/modules/home/index.tsx
@@ -1,5 +1,42 @@
+import { Add } from '@mui/icons-material'
+import { AlertMessage, CustomFAB, ProgressBar } from '@renderer/components'
 import { MainLayout } from '@renderer/layouts/MainLayout'
+import { CreateSerieDialog } from './components'
+import { Fragment, useState } from 'react'
+import { useSeries } from '@renderer/modules/home/hooks/useSeries'
+import { Divider, ListItem, ListItemText } from '@mui/material'
 
 export function Home() {
-  return <MainLayout title="Inicio"></MainLayout>
+  const { data: series, isFetching, refetch: refetchSeries, error } = useSeries()
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <MainLayout title="Inicio">
+      {series?.map((serie) => (
+        <Fragment key={serie.serie_id}>
+          <ListItem>
+            <ListItemText primary={`${serie.serie} ${serie.titulo}`} />
+          </ListItem>
+          <Divider />
+        </Fragment>
+      ))}
+
+      <CreateSerieDialog open={open} handleClose={handleClose} refetchSeries={refetchSeries} />
+
+      <CustomFAB onClick={handleClickOpen}>
+        <Add />
+      </CustomFAB>
+
+      {isFetching && <ProgressBar />}
+      {error && <AlertMessage isError message={error.message} />}
+    </MainLayout>
+  )
 }
